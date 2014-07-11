8e57c7634a3cbdb753f5f6e349fa51104ee69ee8
Sean Brown <inphomercial@gmail.com>
2014-07-11 09:59:25 -0600

    asdf

--- js/Controllers/management_controller.js
+++ js/Controllers/management_controller.js
@@ -1,13 +1,10 @@
 F3ScriptsApp.controller('managementController', function($scope, StorageService) {
 
-	
 	/**
 	* Empties all localstorage keys
 	*/
 	$scope.clear = function()
 	{
-		
-		
 		StorageService.clear();
 	}
 
